f7c05f69ca3af31caf44148e9fcb0f81e7a6f0b7
Stefan <stefan@Stefans-MacBook-Pro.local>
2019-04-26 01:04:43 +0300

    fixing

--- src/slices/Quote.jsx
+++ src/slices/Quote.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
 
-const Quote = ({ input }) => (
-   <div className="section pb-8">
-  <div className="container">
-    <div className="row">
-      <div className="col-sm-12">
-        <div data-wow-delay="0.3s" className="pb-7 custom-header-1 wow fadeInUp">
-          {input.primary.quote_text.text}
-        </div>
-        <div className="text-center">
-          <img width="133" height="28" src="../images/assets/icon-under-title.png"  />
-        </div>
-        
-      </div>
-    </div>
-  </div>
-</div>
+const BlockQuote = styled.blockquote`
+  max-width: ${props => props.theme.maxWidthText};
+`
 
-)
+const Quote = ({ input }) => {
+  if (!input.primary.quote_text) return null
+
+  return
+
+  <BlockQuote>
+    <div dangerouslySetInnerHTML={{ __html: input.primary.quote_text.html }} />
+  </BlockQuote>
+
+}
 
 export default Quote
 
 Quote.propTypes = {
-  input: PropTypes.object
+  input: PropTypes.object.isRequired,
 }
